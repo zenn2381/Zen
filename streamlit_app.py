02f2792a5dcdbcaeeb915dfcc079e455bb005f8e
zenn2381 <a.rayaanggara10@gmail.com>
2025-05-22 14:51:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,7 @@ st.title("Ardi Raya")
 st.write(
 "AKHIRNYA BISA JUGA"
 )
-st.image("Akhirnya.jpg", width=200) 
-st.image("a4fb4fb9-7d8d-483a-8c4f-a2c33a7cc1a7.jpeg", width=200) 
+st.image("Akhirnya.jpg", width=200)
 
 
 st.title("Aplikasi Cihuy") 
@@ -16,3 +15,4 @@ if (angka % 2) == 0:
   st.write(f"{angka} adalah bilangan Genap Eak") 
 else:
   st.write(f"{angka} adalah Bilangan Ganjil Roar") 
+st.image("a4fb4fb9-7d8d-483a-8c4f-a2c33a7cc1a7.jpeg", width=200) 
