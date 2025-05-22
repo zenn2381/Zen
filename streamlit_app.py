c6feb3867374cccdc1c3e88db61075d6519c2888
zenn2381 <a.rayaanggara10@gmail.com>
2025-05-22 14:57:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,5 @@ if (angka % 2) == 0:
 else:
   st.write(f"{angka} Tadaa Jadi Bilangan Ganjil Eak") 
 st.image("a4fb4fb9-7d8d-483a-8c4f-a2c33a7cc1a7.jpeg", width=200) 
+
+st.image("a4fb4fb9-7d8d-483a-8c4f-a2c33a7cc1a7.jpeg", width=200) 
